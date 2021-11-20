a001662f7ce19148d7ed0fd1c64be6735d3bbdc5
RyhamBoutemine <ryhamboutemine@gmail.com>
2021-11-20 13:47:57 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,45 @@
+// function convertminute (a){
+//     return a *=60;
+// }
+// console.log(convertminute(3));
+
+// function area(a,b){
+//     return(a*b);
+// }
+// console.log(area(2,2)) 
+// function convert (a){
+//     return a+=1
+// }
+// console.log(convert(1))
+// function footballPts (a,b,c){
+//     return a=a*3+b*1+0*1
+// }
+// console.log(footballPts(1,2,1))
+// function between (a){
+//     if(a>100 &&a<200){
+//         return true;
+//     }
+//     else{
+//         return false;
+//     }
+// }
+// console.log(between(120));
+// function convertHourMin (a,b){
+//     return a*3600+b*60;
+// }
+// console.log(convertHourMin(1,30));
+// function equalityCheck(a,b){
+//   if(a===b){
+//       return true;
+//   }
+//   else{
+//       return false;
+//   }
+// }
+// console.log(equalityCheck(10,"10"));
+// 
+// var jours = ["samedi","dimanche","lundi"];
+// for(let i=0;i<jours.length;i++){
+//     console.log(jours[i])
+// }
+// let str=    
